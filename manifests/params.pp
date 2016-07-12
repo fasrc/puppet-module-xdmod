1422c671f212c086ee07bfba3eaafbab6f0560fc
nate <n8@adullmoment.com>
2016-07-12 12:14:28 -0400

    make these params

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,19 +2,19 @@
 #
 # Private class.
 #
-class xdmod::params {
-
-  $domain_split        = split($::domain, '[.]')
-  $resource_name       = $domain_split[0]
-  $apache_vhost_name   = "xdmod.${::domain}"
-  $portal_settings     = hiera('xdmod_portal_settings', {})
-  $hierarchies         = hiera('xdmod_hierarchies', [])
-  $group_to_hierarchy  = hiera('xdmod_group_to_hierarchy', {})
-  $user_pi_names       = hiera('xdmod_user_pi_names', [])
-  $rpm_version         = '4.5.2'
-  $real_package_name   = 'xdmod'
-  $rpm_download_source = 'http://heanet.dl.sourceforge.net/project/xdmod/xdmod'
-  $rpm_download_url    = "${rpm_download_source}/${rpm_version}/${real_package_name}-${rpm_version}-1.0.el${::operatingsystemmajrelease}.noarch.rpm"
+class xdmod::params (
+  $domain_split        = split($::domain, '[.]'),
+  $resource_name       = $domain_split[0],
+  $apache_vhost_name   = "xdmod.${::domain}",
+  $portal_settings     = hiera('xdmod_portal_settings', {}),
+  $hierarchies         = hiera('xdmod_hierarchies', []),
+  $group_to_hierarchy  = hiera('xdmod_group_to_hierarchy', {}),
+  $user_pi_names       = hiera('xdmod_user_pi_names', []),
+  $rpm_version         = '4.5.2',
+  $real_package_name   = 'xdmod',
+  $rpm_download_source = 'http://heanet.dl.sourceforge.net/project/xdmod/xdmod',
+  $rpm_download_url    = "${rpm_download_source}/${rpm_version}/${real_package_name}-${rpm_version}-1.0.el${::operatingsystemmajrelease}.noarch.rpm",
+  ){
 
   case $::osfamily {
     'RedHat': {
